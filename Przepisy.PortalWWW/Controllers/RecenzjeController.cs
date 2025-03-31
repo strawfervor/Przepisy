c9c5b78a2abe965fd0841f6337b5eee0958aa13e
strawfervor <strawfervor@gmail.com>
2025-03-31 20:29:43 +0100

    Dodano wyświetlanie recenzji dodanych przez usera

---
+++ Przepisy.PortalWWW/Controllers/RecenzjeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Przepisy.Data.Data;
+
+namespace Przepisy.PortalWWW.Controllers
+{
+    public class RecenzjeController : Controller
+    {
+        private readonly PrzepisyContext _context;
+        public RecenzjeController(PrzepisyContext context)
+        {
+            _context = context;
+        }
+
+        //index będzie wyświetlał recnzje danego użytkownika, domyślnie userId = 1 bo nie ma logowania
+        public async Task<IActionResult> Index()
+        {
+            int userId = 1;
+
+            var recenzje = await _context.Recenzja
+                .Include(r => r.Przepis)
+                .Where(r => r.UzytkownikId == userId)
+                .OrderByDescending(r => r.DataDodania)
+                .ToListAsync();
+
+            return View(recenzje);
+        }
+
+    }
+}
